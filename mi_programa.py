27e8744c7a6ec4ae929ad491760f8525d0076025
Lovemaale <penaalejandrama1980@gmail.com>
2026-08-27 12:21:18 -0500

    prueba

--- mi_programa.py
+++ mi_programa.py
@@ -7,3 +7,9 @@ print("Hola" + mi_nombre)
 numero_1 = 3
 numero_2 = 6
 print(numero_1 + numero_2 )
+
+nombre = input("¿Cual es tu nombre? ")
+peso = float(input("¿Cual es tu peso en Kg? "))
+
+print("Tu nombre es", nombre)
+print("Tu peso es", peso, "Kg")
